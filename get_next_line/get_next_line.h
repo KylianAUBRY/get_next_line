5822ddad16e1a691d3e2aa4ae7f669a60c52c129
Kylian Aubry <kyaubry@c2r1p6.42lehavre.fr>
2023-04-20 20:43:23 +0200

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyaubry <kyaubry@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 14:17:57 by kyaubry           #+#    #+#             */
+/*   Updated: 2023/04/17 21:51:45 by kyaubry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+int		ft_chr(char *s);
+char	*ft_join(char *s1, char *s2);
+void	ft_buf_cut(char *dest);
+int		ft_len(char *s, int j);
+
+#endif
